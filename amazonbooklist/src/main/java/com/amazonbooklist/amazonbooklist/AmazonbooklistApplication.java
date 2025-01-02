5c73e416e983c0b8a75dfacbf45d40b32fbd7357
iftekar2 <iftekar.hossain123@gmail.com>
2025-01-01 21:48:48 -0500

    Adding folders for the projects

---
+++ amazonbooklist/src/main/java/com/amazonbooklist/amazonbooklist/AmazonbooklistApplication.java
@@ -0,0 +1,13 @@
+package com.amazonbooklist.amazonbooklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonbooklistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonbooklistApplication.class, args);
+	}
+
+}
---
+++ amazonbooklist/src/test/java/com/amazonbooklist/amazonbooklist/AmazonbooklistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amazonbooklist.amazonbooklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonbooklistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
